11bc21b43a0305385846212576afa5ba20cb1310
tomm3hgunn <57208697+tomm3hgunn@users.noreply.github.com>
2023-09-23 01:33:22 -0500

    content script requests api, shows rating

--- frontend/src/pages/Content/modules/showData.js
+++ frontend/src/pages/Content/modules/showData.js
@@ -8,11 +8,15 @@ export const showData = () => {
       .then((res) => res.json())
       .then((data) => {
         // Create a new element to display the rating
-        console.log('Data fetched');
-        console.log(data);
         const ratingElement = document.createElement('span');
         ratingElement.className = 'professor-rating';
-        ratingElement.textContent = `Rating: ${data.data.averageRating}, Difficulty: ${data.data.averageDifficulty}`;
+
+        // Check status; if "error," then set text to N/A
+        if (data.status === 'error') {
+          ratingElement.textContent = 'Rating: N/A, Difficulty: N/A';
+        } else {
+          ratingElement.textContent = `Rating: ${data.data.averageRating}, Difficulty: ${data.data.averageDifficulty}`;
+        }
 
         // Append the rating element next to the professor name
         if (element.parentElement) {
@@ -23,19 +27,29 @@ export const showData = () => {
         console.error('Failed to fetch data', error);
       });
   }
-  // Function to execute when the "Search" button is clicked
+
   function showProfessorData() {
     // Find all professor names by title attribute
     const professorElements = document.querySelectorAll(
       'a[title="Click here to get instructor info"]'
     );
-    const firstFiveProfessors = Array.from(professorElements).slice(0, 5); // Select only the first 5 for testing
-    // professorElements.forEach((element) => {
-    firstFiveProfessors.forEach((element) => {
-      const professorName = element.textContent;
-      if (professorName) {
-        console.log(`Fetching data for ${professorName}`);
-        fetchProfessorData(professorName, element);
+    // const firstFiveProfessors = Array.from(professorElements).slice(0, 5); // Select only the first 5
+
+    professorElements.forEach((element, index) => {
+      // Check if the parent <tr> contains "LEC"
+      const parentRow = element.closest('tr');
+      if (parentRow) {
+        const sectionTypeCell = parentRow.querySelector('td:first-child');
+        if (sectionTypeCell && sectionTypeCell.textContent.includes('LEC')) {
+          // Add delay to avoid rate limiting
+          setTimeout(() => {
+            const professorName = element.textContent;
+            if (professorName) {
+              console.log(`Fetching data for ${professorName}`);
+              fetchProfessorData(professorName, element);
+            }
+          }, index * 1000); // ! Remove index for instant results
+        }
       }
     });
   }
