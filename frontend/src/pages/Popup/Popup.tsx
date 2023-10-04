283b602e9b70dcdfe90d90f97d166dce37948f8f
WyattParsons <w631p031@ku.edu>
2023-10-04 12:53:43 -0500

    When enabled: If a classes.ku.edu page does not exist it creates one. If it does exist it does not create a new tab.

--- frontend/src/pages/Popup/Popup.tsx
+++ frontend/src/pages/Popup/Popup.tsx
@@ -1,4 +1,3 @@
-// Hex color: #146091
 import React, { useState } from 'react';
 import './Popup.css';
 
@@ -8,6 +7,23 @@ const Popup: React.FC = () => {
 
     const togglePopup = () => {
         setIsPopupOn(!isPopupOn);
+
+        if(!isPopupOn) {  // When the switch is turned on
+            // Use Chrome tabs API to check if a tab with the URL is open
+            chrome.tabs.query({url: 'https://classes.ku.edu/*'}, (tabs) => {
+                if(tabs.length > 0) {
+                    // If a tab with the URL is open, activate it
+                    chrome.tabs.update(tabs[0].id, {active: true});
+                    if(tabs[0].url !== 'https://classes.ku.edu/') {
+                        // If the URL is not exactly 'https://classes.ku.edu/', redirect to it
+                        chrome.tabs.update(tabs[0].id, {url: 'https://classes.ku.edu/'});
+                    }
+                } else {
+                    // If no tab with the URL is open, create a new one
+                    chrome.tabs.create({url: 'https://classes.ku.edu/'});
+                }
+            });
+        }
     };
 
     return (
