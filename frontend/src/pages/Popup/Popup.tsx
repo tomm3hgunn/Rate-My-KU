170b3b2b955f84768870801ff54070cc046d5beb
WyattParsons <w631p031@ku.edu>
2023-10-22 21:18:50 -0500

    Added settings and stored the values

--- frontend/src/pages/Popup/Popup.tsx
+++ frontend/src/pages/Popup/Popup.tsx
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from 'react';
 import './Popup.css';
 
 const logo = require('../../assets/img/RateMyKU_Logo.png');
+const cog = require('../../assets/img/cog.png');
 
 const Popup: React.FC = () => {
 
-    // Debugging output
+    const [showSettings, setShowSettings] = useState(false); // Added state for toggling settings view
+
+    //! Debugging output
     useEffect(() => {
         chrome.storage.local.get(['isPopupOn'], (result) => {
             console.log('Fetched state from storage:', result.isPopupOn);  // Added log
@@ -15,13 +18,28 @@ const Popup: React.FC = () => {
 
     const [isPopupOn, setIsPopupOn] = useState(false);
 
+    const [settings, setSettings] = useState({
+        showRating: true,
+        showDifficulty: true,
+        showDepartment: true,
+        showWouldTakeAgain: true,
+        showTotalRatings: true
+    });
+
     // Load the saved state from chrome.storage when the component mounts
     useEffect(() => {
-        chrome.storage.local.get(['isPopupOn'], (result) => {
-            console.log('Fetched state from storage:', result.isPopupOn);
-            setIsPopupOn(!!result.isPopupOn);
+        chrome.storage.local.get('settings', (result) => {
+            if (result.settings) {
+                setSettings(result.settings);
+            }
         });
     }, []);
+
+
+    useEffect(() => {
+        chrome.storage.local.set({ settings });
+    }, [settings]);
+
 
     const togglePopup = () => {
         setIsPopupOn(!isPopupOn);
@@ -44,16 +62,80 @@ const Popup: React.FC = () => {
         });
     };
 
+    const toggleSettings = () => {
+        setShowSettings(!showSettings); // Toggle between settings and main view
+    };
+
+    type SettingKeys = 'showRating' | 'showDifficulty' | 'showDepartment' | 'showWouldTakeAgain' | 'showTotalRatings';
+
+    const handleSettingChange = (setting: SettingKeys) => {
+        setSettings(prevSettings => ({
+            ...prevSettings,
+            [setting]: !prevSettings[setting]
+        }));
+    };
+
+
     return (
         <div className={`App ${isPopupOn ? 'active' : ''}`}>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Enable RateMyKU</p>
-                <label className="switch">
-                    <input type="checkbox" onClick={togglePopup} checked={isPopupOn} />
-                    <span className="slider round"></span>
-                </label>
-            </header>
+            {showSettings ? (
+                // Settings view
+                <div className="settings">
+                    <div onClick={toggleSettings} className="back">Back</div>
+                    <h2>Settings</h2>
+                    <label>
+                        Show Rating
+                        <input
+                            type="checkbox"
+                            checked={settings.showRating}
+                            onChange={() => handleSettingChange('showRating')}
+                        />
+                    </label>
+                    <label>
+                        Show Difficulty
+                        <input
+                            type="checkbox"
+                            checked={settings.showDifficulty}
+                            onChange={() => handleSettingChange('showDifficulty')}
+                        />
+                    </label>
+                    <label>
+                        Show Department
+                        <input
+                            type="checkbox"
+                            checked={settings.showDepartment}
+                            onChange={() => handleSettingChange('showDepartment')}
+                        />
+                    </label>
+                    <label>
+                        Show "Would Take Again"
+                        <input type="checkbox"
+                            checked={settings.showWouldTakeAgain}
+                            onChange={() => handleSettingChange('showWouldTakeAgain')}
+                        />
+                    </label>
+                    <label>
+                        Show Total Ratings
+                        <input
+                            type="checkbox"
+                            checked={settings.showTotalRatings}
+                            onChange={() => handleSettingChange('showTotalRatings')}
+                        />
+                    </label>
+
+                </div>
+            ) : (
+                // Main view
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>Enable RateMyKU</p>
+                    <label className="switch">
+                        <input type="checkbox" onClick={togglePopup} checked={isPopupOn} />
+                        <span className="slider round"></span>
+                    </label>
+                    <img src={cog} className="cog-logo" alt="cog logo" onClick={toggleSettings} />
+                </header>
+            )}
         </div>
     );
 };
